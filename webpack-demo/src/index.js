c6ffb2f7f6307ebc93bf0506e7344f28bfd77400
amexabee <amexabee@yahoo.com>
2022-11-03 20:06:37 +0200

    Modify index.html, index.js, and style.css files

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,14 +1,40 @@
-import _ from "lodash";
-import "./style.css";
+import './style.css';
 
-function component() {
-  const element = document.createElement("div");
+const lists = [];
+const registry = () => {
+  const first = { task: 'Task 1', isDone: false, index: 0 };
+  lists.push(first);
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
+  const second = { task: 'Task 2', isDone: false, index: 1 };
+  lists.push(second);
 
-  return element;
-}
+  const third = { task: 'Task 3', isDone: false, index: 2 };
+  lists.push(third);
+};
+const order = (current, next) => current.index - next.index;
 
-document.body.appendChild(component());
+const logLists = () => {
+  const myList = document.getElementById('myList');
+  const ordered = lists.sort(order);
+  ordered.forEach((list) => {
+    const child = document.createElement('li');
+    child.className = 'list-group-item d-flex justify-content-between';
+
+    const myInput = document.createElement('input');
+    myInput.type = 'checkbox';
+    myInput.name = 'name';
+
+    myInput.className = 'form-check-input pull-left';
+    myInput.style.marginRight = '12px';
+    myInput.checked = list.isDone;
+
+    const section = document.createElement('section');
+    section.className = 'fas fa-ellipsis-v pull-right';
+    child.appendChild(myInput);
+    child.appendChild(document.createTextNode(list.task));
+    child.appendChild(section);
+    myList.appendChild(child);
+  });
+};
+registry();
+logLists();
