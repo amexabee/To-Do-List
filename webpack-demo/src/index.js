614524f902b3c19c8322f8e559aecaa7a650e505
amexabee <amexabee@yahoo.com>
2022-11-04 03:29:35 +0200

    Update index.js

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,40 +1,38 @@
-import './style.css';
+import "./style.css";
 
 const lists = [];
 const registry = () => {
-  const first = { task: 'Task 1', isDone: false, index: 0 };
+  const first = { task: "Task 1", isDone: false, index: 0 };
   lists.push(first);
 
-  const second = { task: 'Task 2', isDone: false, index: 1 };
+  const second = { task: "Task 2", isDone: false, index: 1 };
   lists.push(second);
 
-  const third = { task: 'Task 3', isDone: false, index: 2 };
+  const third = { task: "Task 3", isDone: false, index: 2 };
   lists.push(third);
 };
-const order = (current, next) => current.index - next.index;
+const order = (a, b) => a.index - b.index;
 
-const logLists = () => {
-  const myList = document.getElementById('myList');
-  const ordered = lists.sort(order);
-  ordered.forEach((list) => {
-    const child = document.createElement('li');
-    child.className = 'list-group-item d-flex justify-content-between';
+const logItems = () => {
+  const myList = document.getElementById("myList");
+  lists.sort(order).forEach((item) => {
+    const child = document.createElement("input");
+    child.type = "checkbox";
+    child.name = "name";
+    child.className = "form-check-input pull-left";
+    child.style.marginRight = "12px";
+    child.checked = item.isDone;
 
-    const myInput = document.createElement('input');
-    myInput.type = 'checkbox';
-    myInput.name = 'name';
+    const section = document.createElement("section");
+    section.className = "fas fa-ellipsis-v pull-right";
 
-    myInput.className = 'form-check-input pull-left';
-    myInput.style.marginRight = '12px';
-    myInput.checked = list.isDone;
-
-    const section = document.createElement('section');
-    section.className = 'fas fa-ellipsis-v pull-right';
-    child.appendChild(myInput);
-    child.appendChild(document.createTextNode(list.task));
-    child.appendChild(section);
-    myList.appendChild(child);
+    const list = document.createElement("li");
+    list.className = "list-group-item d-flex justify-content-between";
+    list.appendChild(child);
+    list.appendChild(document.createTextNode(item.task));
+    list.appendChild(section);
+    myList.appendChild(list);
   });
 };
 registry();
-logLists();
+logItems();
