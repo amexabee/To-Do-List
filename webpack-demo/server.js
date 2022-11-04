3c60909389134cc5c8f668bbb5784f1d20f655ae
amexabee <amexabee@yahoo.com>
2022-11-04 21:44:04 +0200

    Fix linters issue

--- webpack-demo/server.js
+++ webpack-demo/server.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-unresolved */
+
 const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
