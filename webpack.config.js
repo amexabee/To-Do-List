a822a28bb40791b7918ab7c4c5cdb2e0727eb3a4
amexabee <amexabee@yahoo.com>
2022-11-05 10:43:20 +0200

    Fix display issue of index.html from the /dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,37 @@
 /* eslint-disable import/no-unresolved */
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
+  mode: "production",
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "inline-source-map",
   devServer: {
-    static: './dist',
+    static: "./dist",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
   ],
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
-    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   optimization: {
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
   },
   performance: {
     hints: false,
