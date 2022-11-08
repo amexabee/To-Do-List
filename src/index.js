219fe6ff716257d7d3059b40723a8baa368cfd6c
amexabee <amexabee@yahoo.com>
2022-11-08 15:55:32 +0200

    Modify index.js and style.css

--- src/index.js
+++ src/index.js
@@ -5,59 +5,58 @@ import './style.css';
 
 const addRemove = new AddRemove();
 
-const order = (a, b) => a.index - b.index;
-
 const logList = () => {
   const dash = document.getElementById('dashboard');
   dash.innerHTML = '';
-  addRemove.items.sort(order).forEach((item) => {
-    const lists = document.createElement('li');
-    lists.className =
-      'list-group-item d-flex justify-content-between align-items-center';
+  addRemove.items
+    .sort((a, b) => a.index - b.index)
+    .forEach((item) => {
+      const lists = document.createElement('li');
+      lists.className = 'list-group-item d-flex';
 
-    const child = document.createElement('input');
-    child.type = 'checkbox';
-    child.name = 'name';
-    child.className = 'form-check-input pull-left';
-    child.style.marginRight = '12px';
-    child.checked = item.isDone;
-    child.addEventListener('change', (event) => {
-      new Updates().update(event, item);
+      const child = document.createElement('input');
+      child.type = 'checkbox';
+      child.name = 'name';
+      child.className = 'form-check-input pull-left';
+      child.style.marginRight = '12px';
+      child.checked = item.isDone;
+      child.addEventListener('change', (event) => {
+        new Updates().update(event, item);
+      });
+
+      const txt = document.createElement('span');
+      txt.setAttribute('contenteditable', 'true');
+      txt.setAttribute('id', 'dash');
+      txt.appendChild(document.createTextNode(item.task));
+      txt.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+          item.newTask(txt.innerText);
+          addRemove.edit(item);
+          txt.setAttribute('contenteditable', 'false');
+          txt.setAttribute('contenteditable', 'true');
+        }
+      });
+
+      const close = document.createElement('span');
+      close.className = 'fa fa-trash-o pull-right close';
+      close.addEventListener('click', (e) => {
+        e.preventDefault();
+        addRemove.remove(item);
+        logList();
+      });
+
+      const ellipsis = document.createElement('span');
+      ellipsis.className = 'fas fa-ellipsis-v pull-right';
+      ellipsis.addEventListener('click', (event) => {
+        event.preventDefault();
+        ellipsis.style = 'display:none';
+        close.style = 'display: block';
+      });
+
+      lists.append(child, txt, close, ellipsis);
+
+      dash.appendChild(lists);
     });
-
-    const txt = document.createElement('span');
-    txt.setAttribute('contenteditable', 'true');
-    txt.setAttribute('id', 'dash');
-    txt.appendChild(document.createTextNode(item.task));
-    txt.addEventListener('keypress', (e) => {
-      if (e.key === 'Enter') {
-        item.newTask(txt.innerText);
-        addRemove.edit(item);
-        txt.setAttribute('contenteditable', 'false');
-        txt.setAttribute('contenteditable', 'true');
-      }
-    });
-
-    const close = document.createElement('span');
-    close.className = 'fa fa-trash-o pull-right close';
-    close.addEventListener('click', (e) => {
-      e.preventDefault();
-      addRemove.remove(item);
-      logList();
-    });
-
-    const ellipsis = document.createElement('span');
-    ellipsis.className = 'fas fa-ellipsis-v pull-right';
-    ellipsis.addEventListener('click', (event) => {
-      event.preventDefault();
-      ellipsis.style = 'display:none';
-      close.style = 'display: block';
-    });
-
-    lists.append(child, txt, close, ellipsis);
-
-    dash.appendChild(lists);
-  });
 
   document.getElementById('task').addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
